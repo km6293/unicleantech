19b426116858cf33430dc86dc13ccdcd7b2eb66f
km6293 <alstn7616@naver.com>
2024-09-27 03:00:48 +0900

    fix: src error 임시 수정

---
+++ src/app/page.tsx
@@ -0,0 +1,18 @@
+export default function page() {
+  return (
+    <>
+      {/* <video
+        width="100%"
+        height="100%"
+        autoPlay
+        muted
+        loop
+        preload="auto"
+        style={{ marginTop: "20px", display: "block", pointerEvents: "none" }}
+      >
+        <source src={`${process.env.NEXT_PUBLIC_VIDEO_SRC}`} type="video/mp4" />
+        Your browser does not support the video tag.
+      </video> */}
+    </>
+  );
+}
