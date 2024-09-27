33a422d6838927bc5ced83c5689ce1b0084d767e
km6293 <alstn7616@naver.com>
2024-09-28 07:49:03 +0900

    fix: clean code

--- src/app/_component/index.ts
+++ src/app/_component/index.ts
@@ -1,4 +1,7 @@
 import Footer from "./Footer";
 import Header from "./Header";
+import BackgroundVideo from "./BackgroundVideo";
+import Intro from "./Intro";
+import Quality from "./Quality";
 
-export { Footer, Header };
+export { Footer, Header, BackgroundVideo, Intro, Quality };
--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,24 +1,12 @@
 import style from "./page.module.css";
+import { BackgroundVideo, Intro, Quality } from "./_component";
 
 export default function page() {
   return (
-    <main>
-      <section className={style.videoContainer}>
-        <video className={style.video} autoPlay muted loop preload="auto">
-          <source
-            src={`${process.env.NEXT_PUBLIC_VIDEO_SRC}`}
-            type="video/mp4"
-          />
-          Your browser does not support the video tag.
-        </video>
-
-        <article className={style.videoDetail}>
-          <p>Super Clean - Master</p>
-          <h3>13년간 13,000건의 청소 경력</h3>
-          <h3>총합 1500만원 이상의 전문 장비 사용</h3>
-          <h1>축하드립니다, 청소업체 비교할 시간을 아끼셨네요</h1>
-        </article>
-      </section>
+    <main className={style.mainContainer}>
+      <BackgroundVideo />
+      <Intro />
+      <Quality />
     </main>
   );
 }
