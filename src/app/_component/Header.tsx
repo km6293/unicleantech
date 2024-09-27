9db3dc40d91a35cfa7f5e9ede47f89fb2c6157fd
km6293 <alstn7616@naver.com>
2024-09-27 15:59:13 +0900

    design: header 애니메이션 추가

--- src/app/_component/Header.tsx
+++ src/app/_component/Header.tsx
@@ -1,9 +1,26 @@
+"use client";
+
+import { useEffect, useState } from "react";
 import style from "./header.module.css";
 import Link from "next/link";
 
 export default function Header() {
+  const [isScrolled, setIsScrolled] = useState(false);
+
+  const handleScroll = () => setIsScrolled(window.scrollY > 20);
+
+  useEffect(() => {
+    window.addEventListener("scroll", handleScroll);
+    return () => {
+      window.removeEventListener("scroll", handleScroll);
+    };
+  }, []);
+
   return (
-    <header className={style.header} role="banner">
+    <header
+      className={`${style.header} ${isScrolled ? style.scrolled : ""}`}
+      role="banner"
+    >
       <div className={style.logo}>
         <Link href={"/"}>유니클린텍</Link>
       </div>
