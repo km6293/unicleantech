26cf72279add2e96a3f858edf6912c47d8271272
km6293 <alstn7616@naver.com>
2024-09-28 09:03:17 +0900

    design: client section

---
+++ src/app/_component/Client.tsx
@@ -0,0 +1,13 @@
+import style from "./client.module.css";
+
+export default function Client() {
+  return (
+    <section className={style.clientContainer}>
+      <h2>CLIENT</h2>
+      <h3>청소의 본질에 집중합니다.</h3>
+      <p>
+        신뢰와 실력을 기본적으로 요구하는 업체들과 꾸준히 계약을 맺고 있어요.
+      </p>
+    </section>
+  );
+}
