bd93f655258704358fae1a8548cd49762019d3e1
km6293 <alstn7616@naver.com>
2024-09-28 18:29:06 +0900

    design: 미디어쿼리 완료

--- src/app/_component/Header.tsx
+++ src/app/_component/Header.tsx
@@ -25,8 +25,8 @@ export default function Header() {
         <Link href={"/"}>유니클린텍</Link>
       </div>
       <nav>
-        <Link href={"/about"}> 청소</Link>
-        <Link href={"/about"}> 새집증후군</Link>
+        <Link href={"/"}> 청소</Link>
+        <Link href={"/"}> 새집증후군</Link>
       </nav>
     </header>
   );
