05985a11507d906ca28c7d747b8d610c215b6814
km6293 <alstn7616@naver.com>
2024-09-28 09:03:45 +0900

    design: work section

---
+++ src/app/_component/Work.tsx
@@ -0,0 +1,31 @@
+import style from "./work.module.css";
+
+export default function Work() {
+  return (
+    <section className={style.workContainer}>
+      <h2>WOKR</h2>
+      <h3>유니클린텍 주요업무</h3>
+      <div>
+        <article>
+          <h3>청소</h3>
+          <p>
+            유니클린텍의 자랑입니다, 13년간 13,000건 이상의 청소 경력이면 긴말이
+            필요할까요? 아래 버튼 클릭시 유니클린텍의 청소 철학과 과정, 리뷰,
+            가격에 대해서 상세히 설명해 드려요.
+          </p>
+          <button>자세히 알아보기</button>
+        </article>
+        <article>
+          <h3>코팅</h3>
+          <p>
+            청소에 사용되는 장비와 약품이 얼마나 중요한지 위에서 살펴보셨죠?
+            코팅 또한 마찬가지입니다, 저희는 고가의 장비와 세계에서 인증받은
+            약품을 이용하여 작업자의 안전은 기본이며 소비자의 만족과 건강 또한
+            동시에 만족시켜 드려요.
+          </p>
+          <button>자세히 알아보기</button>
+        </article>
+      </div>
+    </section>
+  );
+}
