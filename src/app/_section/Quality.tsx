528635126fca1f32ab72410e61efe01e04054047
km6293 <alstn7616@naver.com>
2024-10-01 02:35:14 +0900

    fix: quality 중복 처리

--- src/app/_section/Quality.tsx
+++ src/app/_section/Quality.tsx
@@ -2,51 +2,52 @@ import { Video } from "../_component";
 import style from "./quality.module.css";
 
 export default function Quality() {
+  const DATA = [
+    {
+      title: "The highest quality",
+      subtitle: "바닥청소기 하나에 700만원, 비싼 장비에는 다 이유가 있어요.",
+      description:
+        "비싼 장비에는 다 이유가 있습니다, 많은 이윤을 남기기 위해 저렴한 장비를 사용하다 보면 최악의 상황에는 바닥이 손상되거나 청소 또한 완벽히 되지 않을수 있습니다. * 아래 버튼 클릭시 실제 판매 링크로 이동됩니다.",
+      buttonText: "실제 가격을 확인해 보세요!",
+      videoFirst: true,
+    },
+    {
+      title: "The highest quality",
+      subtitle: "바닥청소기 하나에 700만원, 비싼 장비에는 다 이유가 있어요.",
+      description:
+        "비싼 장비에는 다 이유가 있습니다, 많은 이윤을 남기기 위해 저렴한 장비를 사용하다 보면 최악의 상황에는 바닥이 손상되거나 청소 또한 완벽히 되지 않을수 있습니다. * 아래 버튼 클릭시 실제 판매 링크로 이동됩니다.",
+      buttonText: "실제 가격을 확인해 보세요!",
+      videoFirst: false,
+    },
+    {
+      title: "The highest quality",
+      subtitle: "바닥청소기 하나에 700만원, 비싼 장비에는 다 이유가 있어요.",
+      description:
+        "비싼 장비에는 다 이유가 있습니다, 많은 이윤을 남기기 위해 저렴한 장비를 사용하다 보면 최악의 상황에는 바닥이 손상되거나 청소 또한 완벽히 되지 않을수 있습니다. * 아래 버튼 클릭시 실제 판매 링크로 이동됩니다.",
+      buttonText: "실제 가격을 확인해 보세요!",
+      videoFirst: true,
+    },
+  ];
+
   return (
     <section className={style.imageBackground}>
       <div className={style.contentContainer}>
-        <article className={style.content}>
-          <Video src={`${process.env.NEXT_PUBLIC_VIDEO_SRC}`} />
-          <div>
-            <h2>The highest quality</h2>
-            <h3>바닥청소기 하나에 700만원, 비싼 장비에는 다 이유가 있어요.</h3>
-            <p>
-              비싼 장비에는 다 이유가 있습니다, 많은 이윤을 남기기 위해 저렴한
-              장비를 사용하다 보면 최악의 상황에는 바닥이 손상되거나 청소 또한
-              완벽히 되지 않을수 있습니다. * 아래 버튼 클릭시 실제 판매 링크로
-              이동됩니다.
-            </p>
-            <button>실제 가격을 확인해 보세요!</button>
-          </div>
-        </article>
-        <article className={style.content}>
-          <div>
-            <h2>The highest quality</h2>
-            <h3>바닥청소기 하나에 700만원, 비싼 장비에는 다 이유가 있어요.</h3>
-            <p>
-              비싼 장비에는 다 이유가 있습니다, 많은 이윤을 남기기 위해 저렴한
-              장비를 사용하다 보면 최악의 상황에는 바닥이 손상되거나 청소 또한
-              완벽히 되지 않을수 있습니다. * 아래 버튼 클릭시 실제 판매 링크로
-              이동됩니다.
-            </p>
-            <button>실제 가격을 확인해 보세요!</button>
-          </div>
-          <Video src={`${process.env.NEXT_PUBLIC_VIDEO_SRC}`} />
-        </article>
-        <article className={style.content}>
-          <Video src={`${process.env.NEXT_PUBLIC_VIDEO_SRC}`} />
-          <div>
-            <h2>The highest quality</h2>
-            <h3>바닥청소기 하나에 700만원, 비싼 장비에는 다 이유가 있어요.</h3>
-            <p>
-              비싼 장비에는 다 이유가 있습니다, 많은 이윤을 남기기 위해 저렴한
-              장비를 사용하다 보면 최악의 상황에는 바닥이 손상되거나 청소 또한
-              완벽히 되지 않을수 있습니다. * 아래 버튼 클릭시 실제 판매 링크로
-              이동됩니다.
-            </p>
-            <button>실제 가격을 확인해 보세요!</button>
-          </div>
-        </article>
+        {DATA.map((item, index) => (
+          <article className={style.content} key={index}>
+            {item.videoFirst && (
+              <Video src={`${process.env.NEXT_PUBLIC_VIDEO_SRC}`} />
+            )}
+            <div>
+              <h2>{item.title}</h2>
+              <h3>{item.subtitle}</h3>
+              <p>{item.description}</p>
+              <button>{item.buttonText}</button>
+            </div>
+            {!item.videoFirst && (
+              <Video src={`${process.env.NEXT_PUBLIC_VIDEO_SRC}`} />
+            )}
+          </article>
+        ))}
       </div>
     </section>
   );
